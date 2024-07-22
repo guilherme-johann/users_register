d7f4fe1deea5e3aac6e9013ec3645f665db09b13
guilherme-johann <147114001+guilherme-johann@users.noreply.github.com>
2024-07-22 20:46:17 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,33 +1,51 @@
-def line(size=40):
-    return "-" * size
+from exercises.exercise115a.library.interface import *
 
 
-def header(txt):
-    print(line())
-    print(txt.center(len(line())))
-    print(line())
+def fileexistence(name):
+    try:
+        f = open(name, "rt")
+        f.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
 
 
-def menu(data):
-    header("MAIN MENU")
-    c = 1
-    for item in data:
-        print(f"{c} - {item} ")
-        c += 1
-    print(line())
-    option = readinteger("Choose an option:")
-    return option
+def createfile(name):
+    try:
+        f = open(name, "wt+")
+        f.close()
+    except:
+        print("\033[0;31mThere was an error creating the file...\033[m")
+    else:
+        print(f"\033[0;32mFile {name} created with success!\033[m")
 
 
-def readinteger(msg):
-    while True:
+def readfile(name):
+    try:
+        f = open(name, "rt")
+    except:
+        print("\033[0;31mThere was an error reading the file...\033[m")
+    else:
+        header("Registered Users")
+        for line in f:
+            data = line.split(";")
+            data[1] = data[1].replace("\n", "")
+            print(f"{data[0]:<30}{data[1]:>3} years")
+    finally:
+        f.close()
+
+
+def register(file, name="unknown", age=0):
+    try:
+        f = open(file, "at")
+    except:
+        print("\033[0;31mThere was an error while opening the file.\033[m")
+    else:
         try:
-            n = int(input(msg))
-        except (ValueError, TypeError):
-            print("\033[0;31mERROR! Insert a valid integer number\033[m.")
-            continue
-        except KeyboardInterrupt:
-            print(f"\n\033[0;31mThe user preferred not to inform this data.\033[m")
-            return 0
+            f.write(f"{name};{age}\n")
+        except:
+            print("\033[0;31mThere was an error while registering the data.\033[m")
         else:
-            return n
+            print(f"\033[0;32m{name} is now registered!\033[m")
+            f.close()
